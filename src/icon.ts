b92d375058c25494b45ed945b5c22c1ad71396bf
Yahiewi <yahiahenien@gmail.com>
2025-07-23 17:09:37 +0200

    Change export logo

--- src/icon.ts
+++ src/icon.ts
@@ -1,5 +1,5 @@
 import { LabIcon } from '@jupyterlab/ui-components';
-import exportIconStr from '../style/icon/export-urdf.svg';
+import exportIconStr from '../style/icon/export-urdf-plain.svg';
 
 export const exportIcon = new LabIcon({
   name: 'jupytercad:urdf-icon',
