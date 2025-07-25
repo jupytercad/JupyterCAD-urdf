d7b0c4a91e6a603836b37a3f47facd7fc3ae0eb9
Yahiewi <yahiahenien@gmail.com>
2025-07-25 14:03:40 +0200

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { args: 'none' }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off'
+  },
+  ignorePatterns: [
+    'lib/**',
+    'jupytercad_urdf/labextension/**',
+    'node_modules/**'
+  ]
+};
